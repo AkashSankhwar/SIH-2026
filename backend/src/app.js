0b037e1622b948badca37b1ce93a69fc433f8cc3
AkashSankhwar <akashsankhwar70@gmail.com>
2026-08-25 15:12:38 +0530

    complete citizen potal after login

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const authRouter = require("./routes/auth.route.js");
 const incidentRoutes = require("./routes/incident.route.js");
 const alertRoutes = require("./routes/alert.route.js");
 const shelterRouter = require("./routes/shelter.route");
+const helpPostRouter = require("./routes/helppost.route");
 const app=express();
 
 const cors = require("cors");
@@ -14,6 +15,7 @@ app.use("/api/auth",authRouter);
 app.use("/api/incidents", incidentRoutes);
 app.use("/api/alerts", alertRoutes);
 app.use("/api/shelters", shelterRouter);
+app.use("/api/help-posts", helpPostRouter);
 
 app.get("/",(req,res)=>{
     res.send("server is running");
